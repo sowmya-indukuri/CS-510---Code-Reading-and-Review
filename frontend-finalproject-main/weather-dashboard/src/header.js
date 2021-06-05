24287bbad0045d5f4893b4feb484e56c01c8aa05
indukuri <indukuri@pdx.edu>
2021-06-05 12:48:45 -0700

    closes #14

--- frontend-finalproject-main/weather-dashboard/src/header.js
+++ frontend-finalproject-main/weather-dashboard/src/header.js
@@ -1,53 +1,70 @@
-import React, { useState } from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Collapse,NavbarToggler } from "reactstrap";
-import logo from "./logo.png";
-import "./headerstyle.css";
-import "./App.css";
+import React, { useState } from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Collapse,
+  NavbarToggler,
+} from 'reactstrap'
+import logo from './logo.png'
+import './headerstyle.css'
+import './App.css'
 
 //This function is called from homepage where it contains
 //the major route logic and navbar code.
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  
-  const handletoggle = () => {setIsOpen(!isOpen)};
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handletoggle = () => {
+    setIsOpen(!isOpen)
+  }
 
   const headStyle = {
-    color: "white",
-  };
+    color: 'white',
+  }
 
   const headerstyle = {
-    color: "white",  
-  };
+    color: 'white',
+  }
 
   return (
     <div className="container-fluid pad">
-            <Navbar
-              bg="light"
-              variant="light"
-              expand="sm"
-            >  
-            <NavbarBrand style={headStyle} href="/frontend-finalproject/">
-              <img src={logo} width="40"   height="35" alt="weather icon" />
-              <span className="wtitle">&nbsp;Weather Dashboard</span>
-            </NavbarBrand>
-              <NavbarToggler className="toggle" onClick={handletoggle} aria-controls="basic-navbar-nav" 
-              aria-label="Toggle navigation">
-              <div className="animated-icon1"><span></span><span></span><span></span></div>
-              </NavbarToggler>
-             
-              <Collapse id="basic-navbar-nav" isOpen={isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <NavLink className="font-weight-normal" href="/frontend-finalproject/#/About" style={headerstyle}> 
-                    About   
-                  </NavLink>
-                </NavItem>
-              </Nav>
-              </Collapse>
-            </Navbar>
-        </div>
-  );
-};
+      <Navbar bg="light" variant="light" expand="sm">
+        <NavbarBrand style={headStyle} href="/frontend-finalproject/">
+          <img src={logo} width="40" height="35" alt="weather icon" />
+          <span className="wtitle">&nbsp;Weather Dashboard</span>
+        </NavbarBrand>
+        <NavbarToggler
+          className="toggle"
+          onClick={handletoggle}
+          aria-controls="basic-navbar-nav"
+          aria-label="Toggle navigation"
+        >
+          <div className="animated-icon1">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </NavbarToggler>
 
-export default Header;
+        <Collapse id="basic-navbar-nav" isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink
+                className="font-weight-normal"
+                href="/frontend-finalproject/#/About"
+                style={headerstyle}
+              >
+                About
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  )
+}
+export default Header
