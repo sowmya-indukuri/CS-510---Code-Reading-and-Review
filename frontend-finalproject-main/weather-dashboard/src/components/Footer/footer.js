6bda1d1c4566c12f0b7bfc18a271fe81422c8a26
swetha <swetha.venkatesan@gmail.com>
2021-06-05 12:52:16 -0700

    footer js file comments

--- frontend-finalproject-main/weather-dashboard/src/components/Footer/footer.js
+++ frontend-finalproject-main/weather-dashboard/src/components/Footer/footer.js
@@ -1,11 +1,13 @@
+//Footer component for all pages
+
 import React from "react";
-import "./footerstyle.css";
+import "./footerstyle.css"; //css styling file
 
 function Footer() {
   return (
     <div>
       <nav className="footer navbar justify-content-center">
-        <small>© 2020 All rights reserved</small>
+        <small>© 2021 All rights reserved</small>
       </nav>
     </div>
   );
